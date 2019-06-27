85eb59293d4d02b9c1110e143f6058e88925a822
Luca <lucakaufmann92@gmail.com>
2019-06-27 13:55:14 +0300

    Initial Commit

---
+++ YumYum/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  YumYum
+//
+//  Created by Luca Kaufmann on 27.6.2019.
+//  Copyright © 2019 Luca Kaufmann. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
