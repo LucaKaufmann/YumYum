a9a4fb5a580ead1ec564c7a0c61b4f2f7a81dc00
Luca <lucakaufmann92@gmail.com>
2019-07-24 18:51:16 +0300

    fixing realm delete issue, starting cascade delete implementation

---
+++ YumYum/Extensions/NetworkImage.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkImage.swift
+//  YumYum
+//
+//  Created by Luca Kaufmann on 24/07/2019.
+//  Copyright © 2019 Luca Kaufmann. All rights reserved.
+//
+
+import Foundation
---
+++ YumYum/Extensions/String+urlEncode.swift
@@ -0,0 +1,9 @@
+//
+//  String+urlEncode.swift
+//  YumYum
+//
+//  Created by Luca Kaufmann on 24/07/2019.
+//  Copyright © 2019 Luca Kaufmann. All rights reserved.
+//
+
+import Foundation
